7025381bd1d5f1f4290e5251361a5e6fbbc5e38b
Abiwastaken <abi.mukhiya@gmail.com>
2026-01-04 14:23:54 +0545

    property 0.0.2

--- app/src/main/java/com/example/roomsathi/model/PropertyModel.kt
+++ app/src/main/java/com/example/roomsathi/model/PropertyModel.kt
@@ -1,4 +1,15 @@
 package com.example.roomsathi.model
 
-class PropertyModel {
+import android.R
+
+data class PropertyModel
+    (
+    val propertyId: String = "",
+    val userId: String = "",
+    val title: String = "",
+    val location: String = "",
+    val description: String = "",
+    val cost: Double = 0.0,
+    val status: Boolean = false,
+){
 }
--- app/src/main/java/com/example/roomsathi/repository/PropertyRepo.kt
+++ app/src/main/java/com/example/roomsathi/repository/PropertyRepo.kt
@@ -1,4 +1,13 @@
 package com.example.roomsathi.repository
 
+import com.example.roomsathi.model.PropertyModel
+
 interface PropertyRepo {
+
+    fun updateProperty(userId: String, propertyId: String, model: PropertyModel, callback:(Boolean, String)-> Unit)
+    fun deleteProperty(userId: String, propertyId: String,callback: (Boolean, String) -> Unit)
+    fun getPropertybyId(userId: String, callback:(Boolean, String, PropertyModel?)-> Unit)
+    fun addProperty(userId: String,propertyId: String, model: PropertyModel, callback: (Boolean, String)-> Unit)
+
+
 }
